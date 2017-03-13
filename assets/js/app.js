a5d9a605137f5ed61e9bb6d38b295d71dc2f582b
owen <owenthomasclark@me.com>
2017-03-13 15:17:29 +0000

    Amended homepage information layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,14 +79,9 @@ if (!Array.from) {
 
 var messages = Array.from(document.querySelectorAll('.messages li'));
 var viewportHeight = window.innerHeight;
-console.log("Height = " + viewportHeight);
 
 var showHideMessage = function showHideMessage(scrollPosition) {
-  console.log("function ran");
-  console.log(scrollPosition);
-  console.log(messages);
   for (var i = 0; i < messages.length; i++) {
-    console.log("in for loop" + i);
     if (messages[i].offsetTop <= scrollPosition) {
       messages[i].classList.add('js-message');
     } else {
@@ -96,6 +91,7 @@ var showHideMessage = function showHideMessage(scrollPosition) {
 };
 
 window.addEventListener('scroll', function (e) {
+  // changed to pageYOffset from scrollY due to incompatibility with IE
   var last_known_scroll_position = window.pageYOffset + (viewportHeight - 100);
   showHideMessage(last_known_scroll_position);
   console.log(last_known_scroll_position);
